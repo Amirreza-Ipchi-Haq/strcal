c638e5f4b518d78e66d2759ae5fc822e54f5d943
Amirreza Ipchi-e-Haq <ipchia3@gmail.com>
2024-02-21 22:34:55 +0330

    Update SerialCalculator.ino

--- CPlusPlus_ForArduino/strcal/examples/SerialCalculator/SerialCalculator.ino
+++ CPlusPlus_ForArduino/strcal/examples/SerialCalculator/SerialCalculator.ino
@@ -1,28 +1,34 @@
 //(Tip: You can avoid using `strcal::` namespace by using `using namespace strcal;` in the line after including strcal.h (Don't do this if it conflicts with other libraries))
 #include<strcal.h>//https://github.com/Amirreza-Ipchi-Haq/strcal
-String x,y;//Assign 2 strings which store numbers
+String input,x,y;//Assign 3 strings which the first one stores the input and the last 2 store numbers
 char operation;//Assign the operator character
 void setup(){
 	Serial.begin(9600);//Setup serial communication at 9600 bits per second (Default baud rate)
   	return;
 }
 void loop(){
-	Serial.print("The first number: ");//Prompt for the first number
-	while(!Serial.available());//Wait for input
-	x=Serial.readString(),x.trim(),Serial.println(x),Serial.print("The operator: ");//Store the first number, trim it, display it and prompt for the operator
-	while(!Serial.available());//Wait for input
-	operation=Serial.readString()[0],Serial.println(operation),Serial.print("The second number: ");//Store the operator character, display it and prompt for the second number
-	while(!Serial.available());//Wait for input
-	y=Serial.readString(),y.trim(),Serial.println(y);//Store the second number, trim it and display it
-	if(operation!='+'&&operation!='-'&&operation!='*'&&operation!='%'&&operation!='/')//Alert if the operator isn't valid
-		Serial.println("Not a valid operator!");
-	else if(strcal::isnum(x)&&strcal::isnum(y)){//Both numbers are valid
-		x=strcal::calculate(strcal::fixnum(x),strcal::fixnum(y),operation);//Do operation on both numbers in their standard form based on the operator and store it
-		if(x=="")//Alert if the calculation was dividing by 0 (An empty string returned by the `calculate` function indicates such)
+	x="",y="";//Clears both number strings
+	while(!Serial.available());//Waits for input
+	input=Serial.readString();//Reads the operation
+	input.trim();//Removes leading/trailing whitespaces from the input
+	{//Assigns the number strings & the operator in a temporary scope
+		size_t i=0;//Assigns an iterator
+		for(;input[i]!='+'&&(input[i]!='-'||!i)&&input[i]!='*'&&input[i]!='/'&&input[i]!='%';x+=String(input[i]),i++)//Reads the first number string
+			if(i+1==input.length()){//(Have reached the end)
+				Serial.println("Not a valid operation!");
+				return;
+			}
+		x.trim();//Trims the first number string
+		operation=input[i],y=input.substring(i+1);//Reads the operator & the second number string
+	}
+	y.trim();//Trims the second number string
+	if(strcal::isrnum(x)&&strcal::isrnum(y)){//(Both numbers are valid)
+		x=strcal::rcalculate(strcal::fixrnum(x),strcal::fixrnum(y),operation);//Does operation on both numbers in their standard form based on the operator and stores the answer
+		if(x=="")//Alerts if the calculation was dividing by 0 (An empty string returned by the `calculate` function indicates such)
 			Serial.println("Cannot divide by zero!");
-		else//Show the answer
-			Serial.print("The answer: "),Serial.println(x);
-	}else//Tell which numbers are invalid
-		Serial.println(strcal::isnum(x)^strcal::isnum(y)?strcal::isnum(x)?"The first input isn't a number!":"The third input isn't a number!":"The first & the third input aren't numbers!");
+		else//Shows the answer
+			Serial.println(x);
+	}else//Tells which numbers are invalid
+		Serial.println(strcal::isrnum(x)^strcal::isrnum(y)?strcal::isrnum(x)?"The first input isn't a number!":"The third input isn't a number!":"The first & the third input aren't numbers!");
 	return;
 }
